8249b04fde6f01f933bc6563844aa5c882206c9d
liurenjie-3976 <493578750@qq.com>
2023-08-28 14:01:22 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int main(){
+	do{
+		printf("\033[0;31m+***************************电话号码管理软件*********************+\033[0m\n");
+		printf("\033[0;31m|\t\t\t1. 添加					 |\033[0m\n");
+		printf("\033[0;31m|\t\t\t2. 删除					 |\033[0m\n");
+		printf("\033[0;31m|\t\t\t3. 查找					 |\033[0m\n");
+		printf("\033[0;31m|\t\t\t4. 显示所有记录				 |\033[0m\n");
+		printf("\033[0;31m|\t\t\t0. 退出					 |\033[0m\n");
+		printf("\033[0;31m+****************************************************************+\033[0m\n");
+
+		printf("\033[0;31minput your choice#\033[0m");
+		
+		
+	}while(0);
+}
