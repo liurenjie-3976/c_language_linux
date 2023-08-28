4ee50d113496f42f71865deb60c4895ce58f2da3
liurenjie-3976 <493578750@qq.com>
2023-08-28 14:38:47 +0800

    menu

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 
 
 int main(){
+
+    int sel = 0;
+
+
 	do{
 		printf("\033[0;31m+***************************电话号码管理软件*********************+\033[0m\n");
 		printf("\033[0;31m|\t\t\t1. 添加					 |\033[0m\n");
@@ -16,7 +20,16 @@ int main(){
 		printf("\033[0;31m+****************************************************************+\033[0m\n");
 
 		printf("\033[0;31minput your choice#\033[0m");
+		printf("\033[0;33minput your choice#\033[0m");
+		scanf("%d",&sel);
+        if(sel<0 || sel>4){
+            fflush(stdin);// 清空输入缓冲区
+            system("clear");
+            printf("\t\t\33[0;31m输入错误，请重新输入！\033[0m\n");
+            // getchar();
+            continue;
+        }
+        return sel;
 		
-		
-	}while(0);
+	}while(1);
 }
